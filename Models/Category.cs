ba1c8b798809c961a71146493a2a72f27ae1fb49
Pedro Martinelli <p.martinellinunes@gmail.com>
2024-03-26 13:21:47 -0300

    criando mais models

---
+++ Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using Dapper.Contrib.Extensions;
+
+namespace Blog.Models
+{
+    [Table("[Category]")]
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Slug { get; set; } = string.Empty;
+    }
+}
